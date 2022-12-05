f7b96050e830d8d7f99975c65da4be8c631071bb
felipesandoli <cfelipesandoli@gmail.com>
2022-12-05 20:54:39 +0000

    Create initial functions skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 let tasks = [];
+
+function createNewTask() {
+
+}
+
+function displayTaskTemplate() {
+    
+}
+
+function displayTasks() {
+
+}
+
+function setTaskInProgress() {
+
+}
+
+function setTaskComplete() {
+
+}
