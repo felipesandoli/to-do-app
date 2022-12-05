7f11baf3e4d33e9b05aac47e9b383bc21585ee25
felipesandoli <cfelipesandoli@gmail.com>
2022-12-05 21:25:02 +0000

    Display basic new task template when create new task button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@ let tasks = [];
 // Event Listener for the DOM to be loaded, as done in Love Maths walkthrough project
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementById("new-task");
-    console.log(button);
     button.addEventListener("click", displayTaskTemplate);
 })
 
 function displayTaskTemplate() {
-    
+    let taskTemplate = document.createElement("div");
+    taskTemplate.innerHTML = `
+        <input type="text" required></input>
+        <button>Create</button>
+    `;
+    taskTemplate.classList.add("taskTemplate");
+    let toDo = document.getElementById("to-do");
+    toDo.appendChild(taskTemplate);
 }
 
 function createNewTask() {
