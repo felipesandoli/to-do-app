99f76a450f45af672e7adbca64145a23197e257c
felipesandoli <cfelipesandoli@gmail.com>
2022-12-05 21:05:43 +0000

    Add event listener for new task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 let tasks = [];
 
-function createNewTask() {
+// Event Listener for the DOM to be loaded, as done in Love Maths walkthrough project
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.getElementById("new-task");
+    console.log(button);
+    button.addEventListener("click", createNewTask);
+})
 
+function createNewTask() {
+    
 }
 
 function displayTaskTemplate() {
-    
+
 }
 
 function displayTasks() {
